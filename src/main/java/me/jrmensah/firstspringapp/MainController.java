e6c73f3e10e5fe3321c88c26d195246db05f2f6e
jrmensah <jerilynmensah@gmail.com>
2017-11-06 15:05:02 -0500

    SpringBoot App lesson 1

---
+++ src/main/java/me/jrmensah/firstspringapp/MainController.java
@@ -0,0 +1,14 @@
+package me.jrmensah.firstspringapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    public String homePage()
+    {
+        return "index";
+    }
+
+}
